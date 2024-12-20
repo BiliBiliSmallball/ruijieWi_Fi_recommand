dd1fa25c2fb84d90bfd8ac3f01ed11662ef56ff5
ZNCXZX20 <20224301003048@gtxy.edu.cn>
2024-12-20 14:25:36 +0800

    refactor(clash_monitor, main): 优化进程检测和Wi-Fi重连逻辑，改进日志记录

--- clash_monitor.py
+++ clash_monitor.py
@@ -1,27 +1,41 @@
-import subprocess 
+import subprocess
+import os
+import psutil
 
 def is_process_running(process_name): 
-    result = subprocess.run(['tasklist'], stdout=subprocess.PIPE, text=True) 
-    return process_name in result.stdout 
-def test_process(process_name): 
-    if is_process_running(process_name): 
+    for proc in psutil.process_iter(['name']):
+        if proc.info['name'] == process_name:
+            return True
+    return False
+def test_process(process_name):
+    if is_process_running(process_name):
         print(f"{process_name} 正在运行。")
-        return True 
-    else: 
+        return True
+    else:
         print(f"{process_name} 未运行。")
-        return False 
-
-def start_process(process_path): 
-    try: 
-        subprocess.Popen([process_path]) 
-        print(f"启动 {process_path} 成功。") 
-    except Exception as e: 
-        print(f"启动 {process_path} 失败: {e}")
-                                                       
-if __name__ == "__main__": 
-    process_name = "clash-verge.exe"
-    if not test_process(process_name): 
-        start_process("C:\\Program Files\\Clash Verge\\clash-verge.exe")
-    test_process('clash-verge-service.exe') 
+        return False
     
-    input("按下Enter键退出")
+def start_process(process_path):
+    if os.path.isfile(process_path) and os.access(process_path, os.X_OK):
+        try:
+            subprocess.Popen([process_path])
+            print(f"启动 {process_path} 成功。")
+        except FileNotFoundError as e:
+            print(f"启动 {process_path} 失败: 文件未找到 - {e}")
+        except OSError as e:
+            print(f"启动 {process_path} 失败: 操作系统错误 - {e}")
+        except Exception as e:
+            print(f"启动 {process_path} 失败: {e}")
+    
+    def main():
+        process_names = ["clash-verge.exe", "clash-verge-service.exe"]
+        for name in process_names:
+            start_process("C:\\Program Files\\Clash Verge")
+            #start_process("C:\\Program Files\\Clash Verge\\clash-verge.exe")
+        test_process('clash-verge-service.exe') 
+        # if not test_process('clash-verge-service.exe'):
+        #     start_process("C:\\Program Files\\Clash Verge\\clash-verge-service.exe")
+        input("按下Enter键退出")
+    
+    if __name__ == "__main__":
+        main()
