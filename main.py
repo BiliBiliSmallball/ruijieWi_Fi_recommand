400392839f8f18f9d7f69621a91b07dfe83401dc
ZNCXZX20 <20224301003048@gtxy.edu.cn>
2024-12-21 13:26:21 +0800

    重新来过

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 -*- coding: utf-8 -*-
-@Time    : 2024/12/09 10:38
+@Time    : 2024/12/21 13:24
 @File    : wifi自动重连.py
 @autor   : Ender_Zhu
 @Software: vscode
@@ -10,7 +10,6 @@ import subprocess
 import time
 import clash_monitor as clash
 import log_config
-import os
 
 # 定义常量  
 LOG_CLEAR_THRESHOLD = 600
@@ -23,7 +22,6 @@ def is_wifi_connected():
     if "已连接" in result.stdout: 
         return True 
     return False 
-
 def connect_to_wifi(ssid): 
     # 连接到指定的Wi-Fi网络 
     subprocess.run(['netsh', 'wlan', 'connect', f'name={ssid}'])
@@ -35,22 +33,17 @@ def is_clash_running():
     else: 
         log_config.log_message(1, "clash is not running", open(LOG_FILE, "a"),"main.py")
         return False
-
 def main(ssid: str): 
     reconnect_count = 0 
     run_count = 0 
     log_clear_count = log_config.log_clear_tic_get(ERR_LOG_FILE)
-    #判断文件wifi_reconnect_log.txt是否存在，不存在则创建
-    if not os.path.exists(LOG_FILE):
-        with open(LOG_FILE, "w") as log_file:
-            log_file.write("")
-    
+     
     while True: 
-        # 日志时间判断
+        #日志时间判断
         t = time.localtime() 
-        sleep_time = 10 if 14 <= t.tm_hour or t.tm_hour <= 2 else 1200
+        sleep_time = 10 if 14 <= t.tm_hour or t.tm_hour <= 2 or t.tm_hour == 8 or t.tm_hour == 9 else 1200
         
-        # 核心
+        #核心
         if not is_wifi_connected(): 
             print("Wi-Fi已断开连接，尝试重新连接...") 
             connect_to_wifi(ssid) 
@@ -64,7 +57,7 @@ def main(ssid: str):
                 log_config.log_message(1, "Automatic reconnect failed.", open(LOG_FILE, "a"),"main.py")
         else: 
             print("Wi-Fi已连接。") 
-            log_config.log_message(0, "Wi-Fi is connected", open(LOG_FILE, "a"),"main.py") 
+            log_config.log_message(0, "Wi-Fi is connection", open(LOG_FILE, "a"),"main.py")
         
         run_count += 1 
         log_config.log_message(0, f"Run count: {run_count}", open(LOG_FILE, "a"),"main.py")
@@ -73,18 +66,19 @@ def main(ssid: str):
             if not is_clash_running():
                 clash.start_process("C:\\Program Files\\Clash Verge\\clash-verge.exe")
         
-        # 显示
+        #显示
         print(f"重连次数: {reconnect_count}\n") 
         print(f"运行次数: {run_count}\n") 
         print(f"运行时间: {t.tm_hour}:{t.tm_min}:{t.tm_sec}\n") 
         
-        # 日志清理
+        #日志清理
         if run_count > LOG_CLEAR_THRESHOLD: 
             log_config.log_delet(LOG_FILE, ERR_LOG_FILE, log_clear_count) 
             log_clear_count += 1
             run_count = 0 # 重置运行次数 
             
         time.sleep(sleep_time) 
+
 
 if __name__ == "__main__": 
     print("启动Wi-Fi重连服务。按Ctrl+C退出。") 
@@ -94,6 +88,9 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print("\n停止服务") 
         log_config.log_message(1, "The script is terminated by the user\n---------------------------------------", open(LOG_FILE, "a"),"main.py")
-    except Exception as e:
-        print(f"\n未知错误: {e}")
-        log_config.log_message(1, f"Unknown error: {e}\n---------------------------------------", open(LOG_FILE, "a"),"main.py")
+    
+    #请在测试时注释掉下面这一行
+    except :
+       print("\n未知错误。")
+       log_config.log_message(1, "Unknown error\n---------------------------------------", open(LOG_FILE, "a"),"main.py")
+
